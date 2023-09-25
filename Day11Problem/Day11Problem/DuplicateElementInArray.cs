dc7198c1eb4520cd5aba677f952353a33c1f778a
19AmitKumar <ak9978292@gmail.com>
2023-09-25 21:34:32 +0530

    [Amit] Add:Count Duplicate element in Array

---
+++ Day11Problem/Day11Problem/DuplicateElementInArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11Problem
+{
+    internal class DuplicateElementInArray
+    {
+    }
+    class DuplicateElement
+    {
+        public void Element() {
+            int size;
+            int count=0;
+            Console.WriteLine("Enter the size of Array: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("Element of Array: ");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0;i < size; i++)
+            {
+                
+
+                for (int j = i+1; j < size; j++)
+                {
+                    
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+
+                }
+            }
+            Console.WriteLine("Total no of duplicate element in array is: " + count);
+
+        }
+    } 
+}
---
+++ Day11Problem/Day11Problem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice: ");
+                int a=int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                        case 0:
+                        flag = false;
+                        break;
+                        case 1:
+                        DuplicateElement d = new DuplicateElement();
+                        d.Element();
+                        break;
+                        case 2:
+                        UniqueElement u=new UniqueElement();
+                        u.Unique();
+                        break;
+
+
+
+                        Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
